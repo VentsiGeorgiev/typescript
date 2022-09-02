005d4f0de6eb0f398cabbe13178ffa071e65d796
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-02 13:32:45 +0200

    Update todo

--- react-typescript/src/components/TodoItem.tsx
+++ react-typescript/src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Todo } from './model';
 import {AiFillEdit, AiFillDelete} from 'react-icons/ai';
 import {MdDone} from 'react-icons/md';
@@ -14,6 +14,9 @@ type Props = {
 
 function TodoItem({todo, todos, setTodos}: Props) {
 
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
   const handleDone = (id: number) => {
     setTodos(todos.map((todo) => todo.id === id ? {...todo, isDone: !todo.isDone} : todo ));
   };
@@ -21,14 +24,36 @@ function TodoItem({todo, todos, setTodos}: Props) {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(todos.map((todo) => (
+      todo.id === id? {...todo, todo:editTodo} : todo
+    )));
+    setEdit(false);
+  };
+
 
   return (
-    <form className='form-todo'>
+    <form className='form-todo' onSubmit={(e) => handleEdit(e, todo.id)}>
       <div className="todo">
-        <span className={todo.isDone ? 'done' : ''}>{todo.todo}</span>
+
+        {edit 
+          ? (<input 
+            value={editTodo} 
+            onChange={(e) => setEditTodo(e.target.value)}
+            className='edit'
+            />)
+          : (<span className={todo.isDone ? 'done' : ''}>{todo.todo}</span>)
+        }
+
+        
         
       <div>
-        <span className='icon'><AiFillEdit /></span>
+        <span className='icon' onClick={() => {if(!edit && !todo.isDone) {
+          setEdit(!edit);
+        }}}><AiFillEdit /></span>
+
         <span className='icon' onClick={() => handleDelete(todo.id)}><AiFillDelete /></span>
         <span className='icon' onClick={() => handleDone(todo.id)}><MdDone /></span>
       </div>
