e51129fca0db6b06cd521cd454ac25be7e39abd6
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-02 12:40:41 +0200

    Todo List

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Todo } from './components/model';
 import TaskForm from './components/TaskForm';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  console.log(todo);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    
+    if(todo){
+
+      setTodos((prevState) => [...prevState, {id: Date.now(), todo, isDone: false}]);
+      setTodo('');
+    }
+
+    console.log(todos);
+
+  };
 
   return (
     <div className='App'>
       <h1 className='heading'>Task</h1> 
-      <TaskForm todo={todo} setTodo={setTodo}/>
+      <TaskForm todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 };
--- react-typescript/src/components/TaskForm.tsx
+++ react-typescript/src/components/TaskForm.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './TaskForm.style.css';
 
 interface Props{
   todo: string,
-  setTodo: React.Dispatch<React.SetStateAction<string>>
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const TaskForm: React.FC<Props> = ({todo, setTodo}: Props) => {
+const TaskForm: React.FC<Props> = ({todo, setTodo, handleAdd}: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+
   return (
-    <form className='form'>
+    <form className='form' onSubmit={(e) =>{
+      handleAdd(e);
+      inputRef.current?.blur();
+    }}>
       <input
+      ref={inputRef}
        className='form__input' 
        type="text" 
        placeholder='Enter task'
